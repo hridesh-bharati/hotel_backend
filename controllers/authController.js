7ab720800d51627dca77a28b4191c2e2ee2a3dff
Hrideshbharati <116611293+hridesh-bharati@users.noreply.github.com>
2025-04-07 20:58:10 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const User = require("../models/User");
+const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
